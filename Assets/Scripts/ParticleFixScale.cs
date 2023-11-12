7acac91ceb2bd38220a43c39651bd34f3f40283a
Mashinow <90961989+Mashinow@users.noreply.github.com>
2023-11-12 17:52:03 +0300

    some fixes

--- Assets/Scripts/ParticleFixScale.cs
+++ Assets/Scripts/ParticleFixScale.cs
@@ -10,7 +10,9 @@ public class ParticleFixScale : MonoBehaviour
     }
     void Update()
     {
-     //   float parentScaleX = transform.parent.localScale.x;
-        transform.localScale = new Vector3(1f, 1f, 1f);
+        if (transform.localScale.x <= 0f)
+        {
+            transform.localScale = new Vector3(transform.localScale.x * -1f, transform.localScale.y, transform.localScale.z);
+        }
     }
 }
