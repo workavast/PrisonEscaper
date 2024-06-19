76c54b6b0678eab728d3ea37f68f6b19bc70554f
Rodion <workavast@yandex.ru>
2024-06-19 10:52:38 +0300

    create credits

--- Assets/Scripts/UI/UISystem/Screens/UI_MainMenuScreen.cs
+++ Assets/Scripts/UI/UISystem/Screens/UI_MainMenuScreen.cs
@@ -5,18 +5,27 @@ namespace UI
     public class UI_MainMenuScreen : UI_ScreenBase
     {
         [SerializeField] private GameObject settingsScreen;
+        [SerializeField] private GameObject creditsScreen;
         
         public override void InitScreen()
         {
             base.InitScreen();
 
             settingsScreen.SetActive(false);
+            creditsScreen.SetActive(false);
             Cursor.visible = true;
         }
 
         public void _OpenSettingsScreen()
         {
             settingsScreen.SetActive(true);
+            creditsScreen.SetActive(false);
+        }
+        
+        public void _OpenCreditsScreenScreen()
+        {
+            settingsScreen.SetActive(false);
+            creditsScreen.SetActive(true);
         }
     }
 }
